0835547dd0c22ced3d0e9370e2c9e9f0651d6d2c
kenryalonzo <kenryalonzo@gmail.com>
2023-06-13 19:00:03 +0100

    add line after the fonction mapx and free height

--- Codage R.L.E.hs
+++ Codage R.L.E.hs
@@ -33,3 +33,35 @@ mapx f (x : xs) = f(x) : (mapx f xs)
 mapQ :: (a -> b) ->(QuadTree a) -> (QuadTree b)
 mapQ f (Leaf x) = Leaf (f x)
 mapQ f (Node x y z t) = Node (mapQ f x) (mapQ f y) (mapQ f z) (mapQ f t)
+
+--Bon bon bon, maintenant essayons de d'ecrire une fonction pour inverser une image
+
+data Couleur = Blanc | Noir deriving(Show)
+
+-- Desole j'avais oublier qu'il fallait d'abord que je declare le type couleur.
+
+inverse :: (QuadTree Couleur) -> (QuadTree Couleur)
+inverse (Leaf Noir) = Leaf Blanc
+inverse (Leaf Blanc) = Leaf Noir
+inverse (Node x y z t) = Node (inverse x) (inverse y) (inverse z) (inverse t)
+
+-- Bon maintenant ecrivons une fonction quiu vas roter les images au 1/4
+
+rotate :: (QuadTree a) -> (QuadTree a)
+rotate (Leaf x) = Leaf x 
+rotate (Node x y z t) = Node (rotate y) (rotate z) (rotate t) (rotate x)
+
+-- Prope maintenant on peut passer a quelques choses d'un peu plus particulier, bon je vous laisse decouvrir par vous meme.
+
+data Bit = Zero | Un deriving(Show)
+
+-- maintenant passons au choses serieuses pour la suite.
+
+f :: Couleur -> Bit
+f Blanc = Zero
+f Noir = Un
+
+-- Bon bon bon j'espere que vous ne m'envoudrais pas si je code mintenant la fonction, tiens j'ai un idee je vais l'appelle code qqu'est ce que vous en dites?
+
+code :: (QuadTree Couleur) -> (QuadTree Bit)
+code xs = mapQ f xs
